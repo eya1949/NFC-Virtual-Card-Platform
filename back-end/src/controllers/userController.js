013886d3bcfe1081ba21d8f823f338a0070d917a
eya1949 <ayabacharou@gmail.com>
2024-05-11 14:35:21 +0100

    backend[signOut/delete/update User] fontend[profile/adminePrivateRouters/signIn/signOut v-done]

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -65,6 +65,44 @@ export const updateUser = async (req, res, next) => {
     );
     const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
+    console.log(req.params.userId);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * @desc deletUser
+ * @route /api/users/:userId
+ * @method DELETE
+ * @access private
+ */
+
+export const deleteUser = async (req, res, next) => {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * @desc signOut
+ * @route /api/users/signout
+ * @method Post
+ * @access private
+ */
+
+export const signout = (req, res, next) => {
+  try {
+    res
+      .clearCookie('access_token')
+      .status(200)
+      .json('User has been signed out');
   } catch (error) {
     next(error);
   }
--- front-end/src/components/NavBar2.jsx
+++ front-end/src/components/NavBar2.jsx
@@ -2,79 +2,61 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
-import { useSelector, useDispatch } from 'react-redux';
-import { toggleTheme } from '../redux/theme/themeSlice';
-// import { signoutSuccess } from '../redux/user/userSlice';
-// import { useEffect, useState } from 'react';
+import { toggleTheme } from "../redux/theme/themeSlice";
+import { GiShoppingBag } from "react-icons/gi";
+import { signoutSuccess } from "../redux/User/userSlice";
 
 export default function NavBar2() {
   const path = useLocation().pathname;
-  const location = useLocation();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const { currentUser } = useSelector((state) => state.user);
-    const { theme } = useSelector((state) => state.theme);
-  //   const [searchTerm, setSearchTerm] = useState('');
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
 
-  //   useEffect(() => {
-  //     const urlParams = new URLSearchParams(location.search);
-  //     const searchTermFromUrl = urlParams.get('searchTerm');
-  //     if (searchTermFromUrl) {
-  //       setSearchTerm(searchTermFromUrl);
-  //     }
-  //   }, [location.search]);
 
-  //   const handleSignout = async () => {
-  //     try {
-  //       const res = await fetch('/api/user/signout', {
-  //         method: 'POST',
-  //       });
-  //       const data = await res.json();
-  //       if (!res.ok) {
-  //         console.log(data.message);
-  //       } else {
-  //         dispatch(signoutSuccess());
-  //       }
-  //     } catch (error) {
-  //       console.log(error.message);
-  //     }
-  //   };
-
-  //   const handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     const urlParams = new URLSearchParams(location.search);
-  //     urlParams.set('searchTerm', searchTerm);
-  //     const searchQuery = urlParams.toString();
-  //     navigate(`/search?${searchQuery}`);
-  //   };
+    const handleSignout = async () => {
+      try {
+        const res = await fetch('/api/user/signout', {
+          method: 'POST',
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+          dispatch(signoutSuccess());
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
 
   return (
     <Navbar className="border-b-2">
+      <a className="btn btn-ghost text-xl">Tamurt</a>
       <form>
         <TextInput
           type="text"
           placeholder="Search..."
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
-          //   value={searchTerm}
-          //   onChange={(e) => setSearchTerm(e.target.value)}
+        
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
+      <GiShoppingBag  />
         <Button
           className="w-12 h-10 hidden sm:inline"
           color="gray"
           pill
-            onClick={() => dispatch(toggleTheme())}
+          onClick={() => dispatch(toggleTheme())}
         >
-          
           {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
+        
         {currentUser ? (
-        <Dropdown
+          <Dropdown
             arrowIcon={false}
             inline
             label={
@@ -91,26 +73,27 @@ export default function NavBar2() {
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
             <Dropdown.Divider />
-            {/* <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item> */}
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
           </Dropdown>
         ) : (
-        <Link to="/api/auth/signin">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Sign In
-          </Button>
-        </Link>
-         )} 
+          <Link to="/api/auth/signin">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
+        
       </div>
       <Navbar.Collapse>
         <Navbar.Link active={path === "/"} as={"div"}>
-          <Link to="/">Home</Link>
+          <Link to="/">Shop</Link>
         </Navbar.Link>
         <Navbar.Link active={path === "/about"} as={"div"}>
-          <Link to="/about">About</Link>
+          <Link to="/about">About Us</Link>
         </Navbar.Link>
         <Navbar.Link active={path === "/projects"} as={"div"}>
-          <Link to="/projects">Projects</Link>
+          <Link to="/projects">Researchers Hub</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
