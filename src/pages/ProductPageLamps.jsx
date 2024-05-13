527db46a1c6ba80dc10e11d19cff5675d0cbf9a7
ouijdaneznaki <99818947+ouijdaneznaki@users.noreply.github.com>
2024-05-13 15:04:53 +0200

    product page

--- src/pages/ProductPageLamps.jsx
+++ src/pages/ProductPageLamps.jsx
@@ -1,22 +1,110 @@
-
+import React, { useState } from 'react';
 
 function ProductPageLamps() {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  
+
+  const grid1_lamp=[{
+    img: '../public/chaise/lamp/lamp1.jpeg',
+    img_hover:  "../public/chaise/hover_lamp/hover_lamp1.jpeg" ,
+  },
+  {
+    img: '../public/chaise/lamp/lamp2.jpeg',
+    img_hover:  "../public/chaise/hover_lamp/hover_lamp2.jpeg" ,
+  },
+  {
+    img: '../public/chaise/lamp/lamp3.jpeg',
+    img_hover:  "../public/chaise/hover_lamp/hover_lamp3.jpeg" ,
+  },
+  {
+    img: '../public/chaise/lamp/lamp4.jpeg',
+    img_hover:  "../public/chaise/hover_lamp/hover_lamp4.jpeg" ,
+  }
+]
+const grid2_lamp=[{
+  img: '../public/chaise/lamp/lamp5.jpeg',
+  img_hover:  "../public/chaise/hover_lamp/hover_lamp5.jpeg" ,
+},
+{
+  img: '../public/chaise/lamp/lamp6.jpeg',
+  img_hover:  "../public/chaise/hover_lamp/hover_lapm6.jpeg" ,
+},
+{
+  img: '../public/chaise/lamp/lamp7.jpeg',
+  img_hover:  "../public/chaise/hover_lamp/hover_lamp7.jpeg" ,
+},
+]
+
+  const images = [
+    '../public/chaise/lamp/lamp1.jpeg',
+    '../public/chaise/lamp/lamp2.jpeg',
+    '../public/chaise/lamp/lamp3.jpeg',
+    '../public/chaise/lamp/lamp4.jpeg',
+    '../public/chaise/lamp/lamp5.jpeg',
+    '../public/chaise/lamp/lamp6.jpeg',
+    '../public/chaise/lamp/lamp7.jpeg'
+  ];
+
+  const imageHover = [
+    "../public/chaise/hover_lamp/hover_lamp1.jpeg" ,
+    "../public/chaise/hover_lamp/hover_lamp2.jpeg" ,
+    "../public/chaise/hover_lamp/hover_lamp3.jpeg" ,
+    "../public/chaise/hover_lamp/hover_lamp4.jpeg" ,
+    "../public/chaise/hover_lamp/hover_lamp5.jpeg" ,
+    "../public/chaise/hover_lamp/hover_lapm6.jpeg" ,
+    "../public/chaise/hover_lamp/hover_lamp7.jpeg" ,
+  ];
+
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
+
   return (
     <>
-    <div className="grid grid-cols-4 mt-6">
-      <img className="h-72 w-full  border " src="../public/chaise/lamp/lamp1.jpeg" alt="" />
-      <img className="h-72 w-full  border " src="../public/chaise/lamp/lamp2.jpeg" alt="" />
-      <img className="h-72 w-full  border " src="../public/chaise/lamp/lamp3.jpeg" alt="" />
-      <img className="h-72 w-full  border " src="../public/chaise/lamp/lamp4.jpeg" alt="" />
-    </div>
-    <div className="grid grid-cols-3">
-    <img className="w-full  h-72 border "  src="../public/chaise/lamp/lamp5.jpeg" alt="" />
-    <img className="w-full  h-72 border " src="../public/chaise/lamp/lamp6.jpeg" alt="" />
-    <img className="w-full  h-72 border " src="../public/chaise/lamp/lamp3.jpeg" alt="" />
-    </div>
-      
+     
+<div className="grid grid-cols-4 mt-6 border ">
+         {
+          grid1_lamp.map((product,index)=>(
+          <Product product={product} key={index} />
+          ))
+         }
+
+      </div>
+      <div className="grid grid-cols-3 border ">
+         {
+          grid2_lamp.map((product,index)=>(
+          <Product product={product} key={index} />
+          ))
+         }
+
+      </div>
+
     </>
-  )
+  );
 }
 
-export default ProductPageLamps
+function Product({product}) {
+  return (
+    <div className='relative group hover:opacity-100  h-72 '>
+          
+    <img  className='h-72 w-full  group-hover:hidden' src={product.img} />
+
+    <div className='relative top-0 w-full h-full opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+    <img  className='w-full h-full ' src={product.img_hover} />
+    <div className='grid grid-cols-2 w-full justify-between  absolute bottom-0 z-50 gap-2 px-2 py-1'>
+      <button  className="btn   text-black group-hover:  transition duration-200">DISCOVER</button>
+      <button  className="btn  text-black group-hover:  transition duration-200">ADD TO CART</button>
+    </div>
+    </div>
+
+
+  </div>
+  )}
+
+
+
+export default ProductPageLamps;
--- src/pages/Productspage.jsx
+++ src/pages/Productspage.jsx
@@ -1,24 +1,117 @@
-import MainLayout from "../layout/MainLayout";
+
+import React, { useState } from 'react';
 
 function Productspage() {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const grid1_chaise=[{
+    img: '../public/chaise/chaise1.jpeg',
+    img_hover:  "../public/chaise/hover_chaise/hover_chaise1.jpeg" ,
+  },
+  {
+    img: '../public/chaise/chaise5.jpeg',
+    img_hover:  "../public/chaise/hover_chaise/hover_chaise2.jpeg" ,
+  },
+  {
+    img: '../public/chaise/chaise8.jpeg',
+    img_hover:  "../public/chaise/hover_chaise/hover_chaise3.jpeg" ,
+  },
+  {
+    img: '../public/chaise/chaise6.jpeg',
+    img_hover:  "../public/chaise/hover_chaise/hover_chaise4.jpeg" ,
+  }
+  
+]
+
+const grid2_chaise=[{
+  img: '../public/chaise/chaise3.jpeg',
+  img_hover:  "../public/chaise/hover_chaise/hover_chaise5.jpeg" ,
+},
+{
+  img: '../public/chaise/chaise4.jpeg',
+  img_hover:  "../public/chaise/hover_chaise/hover_chaise6.jpeg" ,
+},
+{
+  img: '../public/chaise/chaise7.jpeg',
+  img_hover:  "../public/chaise/hover_chaise/hover_chaise7.jpeg" ,
+}
+]
+  const images = [
+    '../public/chaise/chaise1.jpeg',
+    '../public/chaise/chaise5.jpeg',
+    '../public/chaise/chaise8.jpeg',
+    '../public/chaise/chaise6.jpeg',
+    '../public/chaise/chaise3.jpeg',
+    '../public/chaise/chaise4.jpeg',
+    '../public/chaise/chaise7.jpeg'
+  ];
+
+  const imageHover = [
+    "../public/chaise/hover_chaise/hover_chaise1.jpeg" ,
+    "../public/chaise/hover_chaise/hover_chaise2.jpeg" ,
+    "../public/chaise/hover_chaise/hover_chaise3.jpeg" ,
+    "../public/chaise/hover_chaise/hover_chaise4.jpeg" ,
+    "../public/chaise/hover_chaise/hover_chaise5.jpeg" ,
+    "../public/chaise/hover_chaise/hover_chaise6.jpeg" ,
+    "../public/chaise/hover_chaise/hover_chaise7.jpeg" ,
+
+   
+  ];
+
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
+
   return (
-   
     <>
-     <div className="grid grid-cols-4 mt-6">
-      <img className="h-72 w-full  border " src="../public/chaise/chaise1.jpeg" alt="" />
-      <img className="h-72 w-full  border " src="../public/chaise/chaise5.jpeg" alt="" />
-      <img className="h-72 w-full  border " src="../public/chaise/chaise8.jpeg" alt="" />
-      <img className="h-72 w-full  border " src="../public/chaise/chaise6.jpeg" alt="" />
-    </div>
-    <div className="grid grid-cols-3">
-    <img className="w-full  h-72 border "  src="../public/chaise/chaise3.jpeg" alt="" />
-    <img className="w-full  h-72 border " src="../public/chaise/chaise4.jpeg" alt="" />
-    <img className="w-full  h-72 border " src="../public/chaise/chaise7.jpeg" alt="" />
-    </div>
+      
+      <div className="grid grid-cols-4 mt-6 border ">
+         {
+          grid1_chaise.map((product,index)=>(
+          <Product product={product} key={index} />
+          ))
+         }
+
+      </div>
+      <div className="grid grid-cols-3 border ">
+         {
+          grid2_chaise.map((product,index)=>(
+          <Product product={product} key={index} />
+          ))
+         }
+
+      </div>
+     
     </>
-   
   );
 }
+
+
+
+function Product({product}) {
+  return (
+    <div className='relative group hover:opacity-100 h-72 '>
+          
+    <img  className='h-72 w-full object-cover' src={product.img} />
+    <div className='absolute top-0 w-full h-full opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+    <img  className='w-full h-full object-cover object-center' src={product.img_hover} />
+    <div className='grid grid-cols-2 w-full justify-between  absolute bottom-0 z-50 gap-2 px-2 py-1'>
+      <button  className="btn   text-black group-hover:  transition duration-200">DISCOVER</button>
+      <button  className="btn  text-black group-hover:  transition duration-200">ADD TO CART</button>
+    </div>
+    
+    </div>
+
+
+  </div>)
+}
+
+
+
 
 export default Productspage;
 
@@ -35,31 +128,6 @@ export default Productspage;
 
 
 
- // <div className="carousel w-full">
-      //   <div id="item1" className="carousel-item w-full">
-      //     <img
-      //       src="../Assets/lamp.jpg"
-      //       className="w-full"
-      //     />
-      //   </div>
-      //   <div id="item2" className="carousel-item w-full">
-      //     <img
-      //       src="../Assets/lamp2.jpg"
-      //       className="w-full"
-      //     />
-      //   </div>
-      //   <div id="item3" className="carousel-item w-full  ">
-      //     <img src="../Assets/lamp"  alt="lamp1" className="w-full" />
-      //   </div>
-      // </div>
-      // <div className="flex justify-center w-full py-2 gap-2">
-      //   {/* <a href="#item1" className="btn btn-xs">
-      //     1
-      //   </a>
-      //   <a href="#item2" className="btn btn-xs">
-      //     2
-      //   </a>
-      //   <a href="#item3" className="btn btn-xs">
-      //     3
-      //   </a> */}
-      // </div>
+
+
+ 
